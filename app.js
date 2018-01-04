00baad3af56bef30e0ee929e25065a54e909477f
xuyanyfb <xuyanyfb@etiantian.com>
2018-01-04 17:53:19 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+
+let func = ()=> {}
+const NUM = 55;
+let arr = [1, 2, 3]
+let arrb = arr.map(item => item*2)
+arr.includes(8)
+console.log(new Set(arrb));
+function* func(){}
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry:{
+        app:'./app.js'
+    },
+    output:{
+        filename:'./bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js/,
+                use:{
+                    loader:'babel-loader'
+                },
+                exclude:'/node_modules/'
+            }
+        ]
+    }
+}

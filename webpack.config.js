10a262b60c4747bccd91be8305ba15cceddf2dd6
xuyanyfb <xuyanyfb@etiantian.com>
2018-01-06 14:08:05 +0800

    添加公用代码提取的实例代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
     entry:{
-        app:'./app.js'
+        pageA:'./src/pageA.js'
     },
     output:{
-        filename:'./bundle.js'
+       path:path.resolve(__dirname,'./dest'),
+        filename:'[name].bundle.js',
+        chunkFilename:'[name].chunk.js'
     },
     module:{
         rules:[
@@ -15,5 +20,11 @@ module.exports = {
                 exclude:'/node_modules/'
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'common',
+            minChunks:2
+        })
+    ]
 }

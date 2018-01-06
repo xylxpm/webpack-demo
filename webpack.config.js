4cc16fdb8c93fc9b1b8a41c1a5ec97c7ae3f86e4
xuyanyfb <xuyanyfb@etiantian.com>
2018-01-06 15:17:20 +0800

    添加公用代码提取的实例代码

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ var path = require('path');
 
 module.exports = {
     entry:{
-        pageA:'./src/pageA.js'
+        pageA:'./src/pageA.js',
+        pageB:'./src/pageB.js',
+        vendor:['lodash']
     },
     output:{
        path:path.resolve(__dirname,'./dest'),
@@ -22,9 +24,23 @@ module.exports = {
         ]
     },
     plugins:[
+        //new webpack.optimize.CommonsChunkPlugin({
+        //    names:['vendor','mainfest'],/* 如果其他参数一样，只有name不一样，可以用names写这样的配置。这里和下面两个配置功能一样， */
+        //    minChunks:Infinity
+        //}),
+
         new webpack.optimize.CommonsChunkPlugin({
-            name:'common',
-            minChunks:2
+            name:'vendor',/* 这里指定entry的名字，可以把第三方依赖库和webpack生成的代码打包在一起 */
+            minChunks:Infinity /* Infinity说明不打包任何模块 */
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'mainfest',/* 写一个entry里没有的名字，可以把第三方依赖库和webpack生成的代码区分开 */
+            minChunks:Infinity
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'common',/* 这里想把业务代码中的公共代码提取出来，重复2次就提取 ，需要指定chunk的范围*/
+            minChunks:2,
+            chunks:['pageA','pageB']
         })
     ]
 }

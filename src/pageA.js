946a0fb9a27399299fc71275f36bb17852e0db5a
xuyanyfb <xuyanyfb@etiantian.com>
2018-01-06 16:56:37 +0800

    添加公用代码提取的实例代码

--- src/pageA.js
+++ src/pageA.js
@@ -1,5 +1,16 @@
-import './subPageA';
-import './subPageB';
-import * as _ from 'lodash';
+if(0){
+    require.ensure(['./subPageA'],function(){},'subPageA')
+}else{
+    require.ensure(['./subPageB'],function(){},'subPageB')
+}
+
+//import * as _ from 'lodash';
+//这里修改代码，实现代码分割
+require.ensure(['lodash'],function(){
+    "use strict";
+    var _ = requier('lodash');  /* 上面一个lodash只是加载，这里才会执行。上面那个可以不写，require.ensure([],function(){})  这样再在里面写本行，也是可以的*/
+    _.join([1,2],'23');
+
+},'vendor')
 
 export default 'pageA';

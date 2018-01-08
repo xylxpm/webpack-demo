d041ff76c7064bdbb1a55a571d7b8da8704e142b
xuyanyfb <xuyanyfb@etiantian.com>
2018-01-08 17:02:38 +0800

    1

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 master:基础分支
 01-CommonsChunkPlugin：实现提取共用代码的分支
+02-ensure：代码分割\懒加载
+03-css-style-loader：cssloader
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
+var path = require('path');
+
 module.exports = {
     entry:{
-        app:'./app.js'
+        app:'./src/app.js'
     },
     output:{
-        filename:'./bundle.js'
+        path:path.resolve(__dirname,'./dist'),
+        publicPath:'./dist/',
+        filename:'[name].bundle.js',
+        chunkFilename:'[name].chunk.js'
     },
     module:{
         rules:[
             {
-                test:/\.js/,
-                use:{
-                    loader:'babel-loader'
-                },
-                exclude:'/node_modules/'
+                test: /\.js$/,
+                use:{ loader:'babel-loader' },
+                exclude:'/node_modules/',
+                include: [path.resolve(__dirname, './dist')]
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    {loader:'style-loader/url' },
+                    {loader:'file-loader' }
+                ]
             }
         ]
     }

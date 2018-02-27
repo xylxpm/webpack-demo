45fc67d00b301374183da2ca968330ece27b1eee
xuyanyfb <xuyanyfb@etiantian.com>
2018-02-27 11:43:27 +0800

    1

--- README.txt
+++ README.txt
@@ -4,4 +4,4 @@ master:基础分支
 03-css-style-loader：处理和css相关的东西，比如css，less，sass，提取css
 04-postcss：解决和postcss相关的问题
 05-Tree-shaking，包含js和css的
-06-file-processing:文件的处理（图片，字体，第三方js库）
+06-file-processing:文件的处理（图片，字体，第三方js库），使用webpack生成html
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path');
 var webpack = require('webpack');
-// var PurifyCSSPlugin = require('purifycss-webpack');
-// var glob = require('glob-all');
-var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var PurifyCSSPlugin = require('purifycss-webpack');
+var glob = require('glob-all');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+
 var extractLess = new ExtractTextWebpackPlugin({
     filename: '[name].min.css',
 })
@@ -17,22 +18,22 @@ module.exports = {
     },
     resolve: {
         alias: {
-            jquery$:path.resolve(__dirname,'./src/libs/jquery.min.js')
+            jquery$: path.resolve(__dirname, 'src/libs/jquery.min.js')
         }
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env']
-                        }
-                    }
-                ]
-            },
+            // {
+            //     test: /\.js$/,
+            //     use: [
+            //         {
+            //             loader: 'babel-loader',
+            //             options: {
+            //                 presets: ['env']
+            //             }
+            //         }
+            //     ]
+            // },
             {
                 test: /\.less$/,
                 use: extractLess.extract({
@@ -50,20 +51,20 @@ module.exports = {
                             }
                         },
 
-                        // {
-                        //     loader: 'postcss-loader',
-                        //     options: {
-                        //         ident: 'postcss',
-                        //         plugins: [
-                        //             require('postcss-sprites')({
-                        //                 spritePath: 'dist/assets/imgs/sprites',
-                        //                 retina: true
-                        //             }),
-                        //             require('postcss-cssnext')()
-                        //         ]
-                        //     }
-                        //     /* postcss-loader 合成雪碧图 ,spritePath合成之后的图片存放的位置，retina处理视网膜屏幕使用的图片，注意对应图片要用@2x命名，css中样式宽高除2*/
-                        // },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: [
+                                    require('postcss-sprites')({
+                                        spritePath: 'dist/assets/imgs/sprites',
+                                        retina: true
+                                    }),
+                                    require('postcss-cssnext')()
+                                ]
+                            }
+                            /* postcss-loader 合成雪碧图 ,spritePath合成之后的图片存放的位置，retina处理视网膜屏幕使用的图片，注意对应图片要用@2x命名，css中样式宽高除2*/
+                        },
                         {
                             loader: 'less-loader'
                         }
@@ -73,25 +74,25 @@ module.exports = {
             {
                 test: /\.(png|jpg|jpeg|gif)$/,
                 use: [
-
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            publicPath: '',
-                            outputPath: 'dist/',
-                            useRelativePath: true
-                        }
-                        /* file-loader 处理css中的图片。outputPath指定输出目录，useRelativePath生成相对url地址，publicPath公共路径，最外层的output不能设置公共路径，否则不起作用*/
-                    },
+                    //
                     // {
-                    //     loader: 'url-loader',
+                    //     loader: 'file-loader',
                     //     options: {
-                    //         name: '[name]-[hash:5].[ext]',
-                    //         limit: 1000,
-                    //         outputPath: 'assets/imgs/'
+                    //         publicPath: '',
+                    //         outputPath: 'dist/',
+                    //         useRelativePath: true
                     //     }
-                    //    /* url-loader base64编码。limit 最小容量，name图片名字。配合postcss-loader使用*/
+                    //     /* file-loader 处理css中的图片。outputPath指定输出目录，useRelativePath生成相对url地址，publicPath公共路径，最外层的output不能设置公共路径，否则不起作用*/
                     // },
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name]-[hash:5].[ext]',
+                            limit: 1000,
+                            outputPath: 'assets/imgs/'
+                        }
+                       /* url-loader base64编码。limit 最小容量，name图片名字。配合postcss-loader使用*/
+                    },
                     {
                         loader: 'img-loader',
                         options: {
@@ -102,8 +103,7 @@ module.exports = {
                         /* img-loader 压缩图片。pngquant处理png类型的图片，还有别的参数，参看npmjs站上的说明*/
                     }
                 ]
-            }
-            ,
+            },
             {
                 test: /\.(eot|woff2?|ttf|svg)$/,
                 use: [
@@ -118,6 +118,17 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test:path.resolve(__dirname,'src/app.js'),
+                use:[
+                    {
+                        loader:'imports-loader',
+                        options: {
+                            $: "jquery"
+                        }
+                    }
+                ]
             }
         ]
     }
@@ -125,17 +136,14 @@ module.exports = {
     plugins: [
         extractLess,
 
-        // new PurifyCSSPlugin({
-        //     /* Purifycss 一定要写在 ExtractTextWebpackPlugin后面*/
-        //     paths: glob.sync([
-        //         path.join(__dirname, './*.html'),
-        //         path.join(__dirname, './src/*.js')
-        //     ]),
-        // }),
-        new webpack.ProvidePlugin({
-            $: "jquery"
-        })
-        // ,
-        // new webpack.optimize.UglifyJsPlugin()
+        new PurifyCSSPlugin({
+            /* Purifycss 一定要写在 ExtractTextWebpackPlugin后面*/
+            paths: glob.sync([
+                path.join(__dirname, './*.html'),
+                path.join(__dirname, './src/*.js')
+            ]),
+        }),
+
+         new webpack.optimize.UglifyJsPlugin()
     ]
 }
